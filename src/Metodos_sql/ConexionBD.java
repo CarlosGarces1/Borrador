3ad86351a4e4024a6be4be4329712c5b0bf42001
Carlos Felipe Garces Yepes <64514142+CarlosGarces1@users.noreply.github.com>
2020-05-16 22:34:36 -0500

    Update ConexionBD.java
    Conectar bd correctamente

--- src/Metodos_sql/ConexionBD.java
+++ src/Metodos_sql/ConexionBD.java
@@ -8,7 +8,7 @@ import java.sql.SQLException;
 
 public class ConexionBD {
     
-    public static String url ="jdbc.mysql://localhost/nombre";
+    public static String url ="jdbc:mysql://localhost/unipaz";
     public static String usuario = "root";
     public static String contraseña = "root";
     public static String clase = "com.mysql.jdbc.Driver";
